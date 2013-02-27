a1e5d73929476a821fed977b9144c4f7f31f3070
Marco Tanner <tannerli@arcor.de>
2013-02-27 19:12:31 +0100

    Created django app and first model of db
    Added pyc to gitignore
    Generated models.py from database

---
+++ cygapp/models.py
@@ -0,0 +1,110 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#     * Rearrange models' order
+#     * Make sure each model has one field with primary_key=True
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
+
+from django.db import models
+
+class ComponentHashes(models.Model):
+    component = models.IntegerField(primary_key=True)
+    key = models.IntegerField(primary_key=True)
+    seq_no = models.IntegerField(primary_key=True)
+    pcr = models.IntegerField()
+    algo = models.IntegerField(primary_key=True)
+    hash = models.TextField() # This field type is a guess.
+    class Meta:
+        db_table = u'component_hashes'
+
+class Components(models.Model):
+    id = models.IntegerField(primary_key=True)
+    vendor_id = models.IntegerField()
+    name = models.IntegerField()
+    qualifier = models.IntegerField(null=True, blank=True)
+    class Meta:
+        db_table = u'components'
+
+class DeviceInfos(models.Model):
+    device = models.IntegerField(primary_key=True)
+    time = models.IntegerField(primary_key=True)
+    product = models.IntegerField(null=True, blank=True)
+    count = models.IntegerField(null=True, blank=True)
+    count_update = models.IntegerField(null=True, blank=True)
+    count_blacklist = models.IntegerField(null=True, blank=True)
+    flags = models.IntegerField(null=True, blank=True)
+    class Meta:
+        db_table = u'device_infos'
+
+class Devices(models.Model):
+    id = models.IntegerField(primary_key=True)
+    value = models.TextField() # This field type is a guess.
+    class Meta:
+        db_table = u'devices'
+
+class FileHashes(models.Model):
+    file = models.IntegerField(primary_key=True)
+    directory = models.IntegerField(null=True, primary_key=True, blank=True)
+    product = models.IntegerField(primary_key=True)
+    key = models.IntegerField(null=True, blank=True)
+    algo = models.IntegerField(primary_key=True)
+    hash = models.TextField() # This field type is a guess.
+    class Meta:
+        db_table = u'file_hashes'
+
+class Files(models.Model):
+    def __unicode__(self):
+        return self.path
+    id = models.IntegerField(primary_key=True)
+    type = models.IntegerField()
+    path = models.TextField()
+    class Meta:
+        db_table = u'files'
+
+class KeyComponent(models.Model):
+    key = models.IntegerField(primary_key=True)
+    component = models.IntegerField(primary_key=True)
+    depth = models.IntegerField(null=True, blank=True)
+    seq_no = models.IntegerField(null=True, blank=True)
+    class Meta:
+        db_table = u'key_component'
+
+class Keys(models.Model):
+    id = models.IntegerField(primary_key=True)
+    keyid = models.TextField() # This field type is a guess.
+    owner = models.TextField()
+    class Meta:
+        db_table = u'keys'
+
+class Packages(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    class Meta:
+        db_table = u'packages'
+
+class ProductFile(models.Model):
+    product = models.IntegerField(primary_key=True)
+    file = models.IntegerField(primary_key=True)
+    measurement = models.IntegerField(null=True, blank=True)
+    metadata = models.IntegerField(null=True, blank=True)
+    class Meta:
+        db_table = u'product_file'
+
+class Products(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    class Meta:
+        db_table = u'products'
+
+class Versions(models.Model):
+    id = models.IntegerField(primary_key=True)
+    package = models.IntegerField()
+    product = models.IntegerField()
+    release = models.TextField()
+    security = models.IntegerField(null=True, blank=True)
+    time = models.IntegerField(null=True, blank=True)
+    class Meta:
+        db_table = u'versions'
+
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# Django settings for cygnetss project.
+# Django settings for cygnet project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'ipsec.config.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'cygnetss.urls'
+ROOT_URLCONF = 'cygnet.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -116,9 +116,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'cygapp',
 )
 
 # A sample logging configuration. The only tangible logging
