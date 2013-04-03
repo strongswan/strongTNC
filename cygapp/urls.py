3ecce27e736474ec729fd271c6a9dc864e90c108
Marco Tanner <tannerli@arcor.de>
2013-04-03 16:54:02 +0200

    Fixed deprecation warnings described in #30

--- cygapp/urls.py
+++ cygapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
 from django.contrib import admin
 from cygapp.models import *
