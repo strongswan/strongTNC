4032dae736114d153861a877a336e085d30a0d7a
Marco Tanner <tannerli@arcor.de>
2013-04-24 11:27:06 +0200

    Refacored unit tests (imports)

--- cygapp/tests.py
+++ cygapp/tests.py
@@ -6,78 +6,80 @@ Unit tests for the django app cygnet are specified in this file
 
 from django.test import TestCase
 from datetime import datetime, timedelta
-import cygapp.models as m
-import cygapp.views as v
+from cygapp.models import (File, WorkItem, Device, Group, Product, Measurement,
+    Policy, Enforcement, Action, Package, Directory, Version, Identity, Result)
+from views import generate_results
+
 
 
 class cygappTest(TestCase):
     def setUp(self):
-        p = m.Product.objects.create(name='Fancy OS 3.14')
-        device = m.Device.objects.create(value='def', description='Test Device', product=p)
+        p = Product.objects.create(name='Fancy OS 3.14')
+        device = Device.objects.create(value='def', description='Test Device', product=p)
         
-        g1 = m.Group.objects.create(name='ROOT')
-        g11 = m.Group.objects.create(name='B1.1', parent=g1)
-        m.Group.objects.create(name='L1.2', parent=g1)
-        g13 = m.Group.objects.create(name='B1.3', parent=g1)
-        g111 = m.Group.objects.create(name='B1.1.1', parent=g11)
-        m.Group.objects.create(name='L1.1.1.1', parent=g111)
-        g1112 = m.Group.objects.create(name='L1.1.1.2', parent=g111)
-        g131 = m.Group.objects.create(name='L1.3.1', parent=g13)
-        m.Group.objects.create(name='L1.3.2', parent=g13)
+        g1 = Group.objects.create(name='ROOT')
+        g11 = Group.objects.create(name='B1.1', parent=g1)
+        Group.objects.create(name='L1.2', parent=g1)
+        g13 = Group.objects.create(name='B1.3', parent=g1)
+        g111 = Group.objects.create(name='B1.1.1', parent=g11)
+        Group.objects.create(name='L1.1.1.1', parent=g111)
+        g1112 = Group.objects.create(name='L1.1.1.2', parent=g111)
+        g131 = Group.objects.create(name='L1.3.1', parent=g13)
+        Group.objects.create(name='L1.3.2', parent=g13)
 
         device.groups.add(g1112)
         device.groups.add(g131)
         device.save()
 
-        m.Policy.objects.create(name='bash',type=1,argument='/bin/bash',fail=3,noresult=0)
-        m.Policy.objects.create(name='usrbin',type=2,argument='/usr/bin/',fail=4,noresult=1)
-        m.Policy.objects.create(name='ports',type=3,argument='0-1024',fail=0,noresult=0)
+        Policy.objects.create(name='bash',type=1,argument='/bin/bash',fail=3,noresult=0)
+        Policy.objects.create(name='usrbin',type=2,argument='/usr/bin/',fail=4,noresult=1)
+        Policy.objects.create(name='ports',type=3,argument='0-1024',fail=0,noresult=0)
 
-        lib = m.Package.objects.create(name='libstrongswan')
-        ss = m.Package.objects.create(name='strongswan')
-        ss_nm = m.Package.objects.create(name='strongswan-nm')
-        ss_dbg = m.Package.objects.create(name='strongswan-dbg')
-        ss_ike = m.Package.objects.create(name='strongswan-ikev1')
+        lib = Package.objects.create(name='libstrongswan')
+        ss = Package.objects.create(name='strongswan')
+        ss_nm = Package.objects.create(name='strongswan-nm')
+        ss_dbg = Package.objects.create(name='strongswan-dbg')
+        ss_ike = Package.objects.create(name='strongswan-ikev1')
 
-        m.Version.objects.create(time=datetime.today(), product=p, package=lib, release='1.1')
-        m.Version.objects.create(time=datetime.today(), product=p, package=ss, release='0.9')
-        m.Version.objects.create(time=datetime.today(), product=p, package=ss_nm, release='3.1')
-        m.Version.objects.create(time=datetime.today(), product=p, package=ss_dbg, release='2.3')
-        m.Version.objects.create(time=datetime.today(), product=p, package=ss_ike, release='1.1')
+        Version.objects.create(time=datetime.today(), product=p, package=lib, release='1.1')
+        Version.objects.create(time=datetime.today(), product=p, package=ss, release='0.9')
+        Version.objects.create(time=datetime.today(), product=p, package=ss_nm, release='3.1')
+        Version.objects.create(time=datetime.today(), product=p, package=ss_dbg, release='2.3')
+        Version.objects.create(time=datetime.today(), product=p, package=ss_ike, release='1.1')
 
-        dir = m.Directory.objects.create(path='/bin')
-        m.File.objects.create(name='bash', directory=dir)
+        dir = Directory.objects.create(path='/bin')
+        File.objects.create(name='bash', directory=dir)
 
     def test_getGroupSet(self):
-        device = m.Device.objects.get(value='def')
+        device = Device.objects.get(value='def')
 
         groups = device.get_group_set()
         self.assertEqual(6, len(groups))
 
-        device.groups.add(m.Group.objects.get(name='B1.1.1'))
-        device.groups.add(m.Group.objects.get(name='L1.3.2'))
+        device.groups.add(Group.objects.get(name='B1.1.1'))
+        device.groups.add(Group.objects.get(name='L1.3.2'))
         
         groups = device.get_group_set()
         self.assertEqual(7, len(groups))
 
     def test_create_work_items(self):
 
-        g = m.Group.objects.get(name='B1.1.1')
-        p = m.Policy.objects.get(name='bash')
-        m.Enforcement.objects.create(group=g, policy=p, max_age=3)
+        g = Group.objects.get(name='B1.1.1')
+        p = Policy.objects.get(name='bash')
+        Enforcement.objects.create(group=g, policy=p, max_age=3)
 
-        g = m.Group.objects.get(name='L1.3.1')
-        p = m.Policy.objects.get(name='usrbin')
-        m.Enforcement.objects.create(group=g, policy=p, max_age=2)
+        g = Group.objects.get(name='L1.3.1')
+        p = Policy.objects.get(name='usrbin')
+        Enforcement.objects.create(group=g, policy=p, max_age=2)
 
-        user = m.Identity()
+        user = Identity()
         user.type = 1
         user.data = 'foobar'
         user.save()
         
-        device = m.Device.objects.get(value='def')
+        device = Device.objects.get(value='def')
 
-        measurement = m.Measurement()
+        measurement = Measurement()
         measurement.device = device
         measurement.time = datetime.today()
         measurement.connectionID = 123
@@ -86,7 +88,7 @@ class cygappTest(TestCase):
 
         device.create_work_items(measurement)
 
-        items = m.WorkItem.objects.filter(measurement=measurement)
+        items = WorkItem.objects.filter(measurement=measurement)
         self.assertEqual(2, len(items))
 
         item = items[0]
@@ -110,60 +112,60 @@ class cygappTest(TestCase):
     def test_actionInheritance(self):
 
         #bash : fail=3 noresult=0
-        g = m.Group.objects.get(name='B1.1.1')
-        p = m.Policy.objects.get(name='bash')
-        e1 = m.Enforcement(group=g, policy=p, max_age=3)
+        g = Group.objects.get(name='B1.1.1')
+        p = Policy.objects.get(name='bash')
+        e1 = Enforcement(group=g, policy=p, max_age=3)
         e1.fail = None
         e1.noresult = None
         e1.save()
 
-        e1 = m.Enforcement.objects.get(group=g, policy=p)
+        e1 = Enforcement.objects.get(group=g, policy=p)
 
         #usrbin: fail=4 noresult=1
-        g = m.Group.objects.get(name='L1.3.1')
-        p = m.Policy.objects.get(name='usrbin')
-        e2 = m.Enforcement(group=g, policy=p, max_age=2)
+        g = Group.objects.get(name='L1.3.1')
+        p = Policy.objects.get(name='usrbin')
+        e2 = Enforcement(group=g, policy=p, max_age=2)
         e2.fail = 3
         e2.noresult = 0
         e2.save()
 
-        user = m.Identity.objects.create(data='foobar')
+        user = Identity.objects.create(data='foobar')
         
-        device = m.Device.objects.get(value='def')
+        device = Device.objects.get(value='def')
 
-        measurement = m.Measurement.objects.create(device=device,
+        measurement = Measurement.objects.create(device=device,
                 time=datetime.today(), connectionID=123, identity=user)
 
         device.create_work_items(measurement)
 
-        item = m.WorkItem.objects.get(measurement=measurement, enforcement=e1)
+        item = WorkItem.objects.get(measurement=measurement, enforcement=e1)
         self.assertEqual(3, item.fail)
         self.assertEqual(0, item.noresult)
 
         
-        item = m.WorkItem.objects.get(measurement=measurement, enforcement=e2)
+        item = WorkItem.objects.get(measurement=measurement, enforcement=e2)
         self.assertEqual(3, item.fail)
         self.assertEqual(0, item.noresult)
 
 
     def test_is_due_for(self):
-        g = m.Group.objects.get(name='L1.3.1')
-        p = m.Policy.objects.get(name='usrbin')
-        user = m.Identity.objects.create(data='foobar')
-        device = m.Device.objects.get(value='def')
-        e = m.Enforcement.objects.create(group=g, policy=p, max_age=2)
+        g = Group.objects.get(name='L1.3.1')
+        p = Policy.objects.get(name='usrbin')
+        user = Identity.objects.create(data='foobar')
+        device = Device.objects.get(value='def')
+        e = Enforcement.objects.create(group=g, policy=p, max_age=2)
 
         #No Measurement yet
         self.assertEqual(True, device.is_due_for(e))
 
         #Measurement yields no results for policy
-        meas = m.Measurement.objects.create(device=device, time=datetime.today(),
+        meas = Measurement.objects.create(device=device, time=datetime.today(),
                 connectionID=123, identity=user)
         self.assertEqual(True, device.is_due_for(e))
 
         #Measurement is too old
-        m.Result.objects.create(policy=p, measurement=meas, result='OK',
-                recommendation = m.Action.ALLOW)
+        Result.objects.create(policy=p, measurement=meas, result='OK',
+                recommendation = Action.ALLOW)
 
         meas.time -= timedelta(days=4)
         meas.save()
@@ -182,33 +184,33 @@ class cygappTest(TestCase):
 
     def test_generate_results(self):
 
-        g = m.Group.objects.get(name='B1.1.1')
-        p1 = m.Policy.objects.get(name='bash')
-        e1 = m.Enforcement.objects.create(group=g, policy=p1, max_age=3)
+        g = Group.objects.get(name='B1.1.1')
+        p1 = Policy.objects.get(name='bash')
+        e1 = Enforcement.objects.create(group=g, policy=p1, max_age=3)
 
-        g = m.Group.objects.get(name='L1.3.1')
-        p2 = m.Policy.objects.get(name='usrbin')
-        e2 = m.Enforcement.objects.create(group=g, policy=p2, max_age=2)
+        g = Group.objects.get(name='L1.3.1')
+        p2 = Policy.objects.get(name='usrbin')
+        e2 = Enforcement.objects.create(group=g, policy=p2, max_age=2)
 
-        device = m.Device.objects.get(value='def')
-        user = m.Identity.objects.create(data='foobar')
-        measurement = m.Measurement.objects.create(device=device,
+        device = Device.objects.get(value='def')
+        user = Identity.objects.create(data='foobar')
+        measurement = Measurement.objects.create(device=device,
                 time=datetime.today(), connectionID=123, identity=user)
         
-        m.WorkItem.objects.create(measurement=measurement, argument='asdf',
+        WorkItem.objects.create(measurement=measurement, argument='asdf',
                 fail=3, noresult=0, result='OK', recommendation=0, enforcement=e1,
                 type=1)
-        m.WorkItem.objects.create(measurement=measurement, argument='blubber',
+        WorkItem.objects.create(measurement=measurement, argument='blubber',
                 fail=3, noresult=0, result='FAIL', recommendation=3, enforcement=e2,
                 type=2)
 
-        v.generate_results(measurement)
+        generate_results(measurement)
 
-        result = m.Result.objects.get(measurement=measurement, policy=p1)
+        result = Result.objects.get(measurement=measurement, policy=p1)
         self.assertEqual('OK', result.result)
         self.assertEqual(0, result.recommendation)
 
-        result = m.Result.objects.get(measurement=measurement, policy=p2)
+        result = Result.objects.get(measurement=measurement, policy=p2)
         self.assertEqual('FAIL', result.result)
         self.assertEqual(3, result.recommendation)
 
@@ -218,10 +220,10 @@ class cygappTest(TestCase):
         pass
 
     def test_policy_file_protection(self):
-        file = m.File.objects.get(name='bash')
+        file = File.objects.get(name='bash')
 
-        policy = m.Policy.objects.create(name='binbash', type=1, argument='%d'%file.id,
-                file=file, fail=m.Action.BLOCK, noresult=m.Action.ALLOW)
+        policy = Policy.objects.create(name='binbash', type=1, argument='%d'%file.id,
+                file=file, fail=Action.BLOCK, noresult=Action.ALLOW)
 
         from django.db.models.deletion import ProtectedError
         self.assertRaises(ProtectedError, file.delete)
@@ -229,11 +231,11 @@ class cygappTest(TestCase):
         file.delete()
 
     def test_policy_dir_protection(self):
-        dir = m.Directory.objects.get(path='/bin')
+        dir = Directory.objects.get(path='/bin')
 
-        policy = m.Policy.objects.create(name='binhashes', type=2,
-                argument='%d'%dir.id, dir=dir, fail=m.Action.BLOCK,
-                noresult=m.Action.ALLOW)
+        policy = Policy.objects.create(name='binhashes', type=2,
+                argument='%d'%dir.id, dir=dir, fail=Action.BLOCK,
+                noresult=Action.ALLOW)
 
         from django.db.models.deletion import ProtectedError
         self.assertRaises(ProtectedError, dir.delete)
