6bf17495090ccf8d2655fa960adb9a4bcfacdae4
Marco Tanner <tannerli@arcor.de>
2013-05-29 12:38:10 +0200

    Added command to manage.py: setpassword
    Update for tannerli/cygnet-doc#69
    
    This might replace the view entirely, I think it would be enough
    
    View the deployment manual on how to use the command

---
+++ cygapp/management/commands/setpassword.py
@@ -0,0 +1,33 @@
+from getpass import getpass
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    help = 'Get or create cygnet-user and set password interactively'
+    args = '[password]'
+
+    def handle(self, *args, **kwargs):
+        if len(args) > 1: raise CommandError('Too many arguments')
+
+        self.stdout.write('looking for cygnet user in database...')
+
+        user, new = User.objects.get_or_create(username='cygnet-user')
+
+        if new:
+            self.stdout.write('... not found: creating new.')
+
+        pwd = ''
+        if len(args) > 0:
+            pwd = args[0]
+
+        if pwd == '':
+            try:
+                pwd = getpass('\nplease enter a new password for cygnet-user: ')
+            except KeyboardInterrupt:
+                self.stdout.write('\nabort')
+                return
+
+        user.set_password(pwd)
+        user.save()
+
+        self.stdout.write('password updated succesfully')
