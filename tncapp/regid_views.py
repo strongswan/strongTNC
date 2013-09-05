54cf1ff66ceeed103b1ec8cd8c394ab8a0de3937
Andreas Steffen <andreas.steffen@strongswan.org>
2013-09-05 11:17:10 +0200

    Fixed search and replace error

--- tncapp/regid_views.py
+++ tncapp/regid_views.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2013 Andreas Steffen
 # HSR University of Applied Sciences Rapperswil
 #
-# This tag is part of strongTNC.  strongTNC is free software: you can
+# This file is part of strongTNC.  strongTNC is free software: you can
 # redistribute it and/or modify it under the terms of the GNU Affero General
 # Public License as published by the Free Software Foundation, either version 3
 # of the License, or (at your option) any later version.
