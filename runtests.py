6bd2870f81ef0590bbaab73942bffd16dd476d7b
Danilo Bargen <mail@dbrgn.ch>
2014-02-20 00:49:47 +0100

    Added support for coverage measurements

--- runtests.py
+++ runtests.py
@@ -2,10 +2,23 @@
 import os
 import sys
 import pytest
+import coverage
 
 if __name__ == '__main__':
+    # Environment variables
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+    # Start coverage tracking
+    cov = coverage.coverage()
+    cov.start()
+
+    # Run pytest
     if len(sys.argv) > 1:
         pytest.main(sys.argv)
     else:
         pytest.main(['tests'])
+
+    # Show coverage report
+    cov.stop()
+    cov.save()
+    cov.report()
