d31091202a8752a56b817dba26ffeeb26434cdc7
Danilo Bargen <mail@dbrgn.ch>
2014-02-20 00:04:38 +0100

    Moved configuration into separate package
    This is a recommended directory organization pattern and is easier to
    manage than having the ``settings.py`` files directly in the root
    package.

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ DATABASES = {
     },
 }
 
-DATABASE_ROUTERS = ['router.DBRouter']
+DATABASE_ROUTERS = ['config.router.DBRouter']
 
 LOGIN_URL = '/login'
 
@@ -121,7 +121,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'tncapp.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
