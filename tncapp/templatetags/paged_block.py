90afa165d70dd1eea871065929e1cd2ce5d4debb
Jonas Furrer <jonas@sequenz.net>
2014-04-27 01:07:26 +0200

    Added ajax based paging

---
+++ tncapp/templatetags/paged_block.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('tncapp/paged_block.html')
+def paged_block(template_name, list_producer, stat_producer, var_name, page_size, with_filter=False):
+    return {
+        'template_name': template_name,
+        'list_producer': list_producer,
+        'stat_producer': stat_producer,
+        'var_name': var_name,
+        'page_size': page_size,
+        'with_filter': with_filter
+    }
