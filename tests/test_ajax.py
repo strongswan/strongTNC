6726d97933d0d712dd5f38117c5db8a05752fba4
Jonas Furrer <jonas@sequenz.net>
2014-04-26 09:49:55 +0200

    Added ajax autocomplete for the directories dropdown in the policy view
    tnc-ba/strongTNC#134

--- tests/test_ajax.py
+++ tests/test_ajax.py
@@ -10,7 +10,7 @@ from tncapp import models
 
 
 @pytest.fixture
-def files_test_data(transactional_db):
+def files_and_directories_test_data(transactional_db):
     """
     Create Directory and File objects in the test database.
     """
@@ -20,6 +20,8 @@ def files_test_data(transactional_db):
     usr_dir = models.Directory.objects.create(path='/usr')
     usr_bin_dir = models.Directory.objects.create(path='/usr/bin')
     etc_dir = models.Directory.objects.create(path='/etc')
+    models.Directory.objects.create(path='/system/lib/etc')
+    models.Directory.objects.create(path='/usr/lib')
 
     # Files
     for name in ['rootfile', 'etcetera']:
@@ -34,14 +36,13 @@ def files_test_data(transactional_db):
 
 
 @pytest.fixture
-def get_completions(client, files_test_data):
+def get_completions(client, files_and_directories_test_data):
     """
     Fixture that provides a parametrized function that queries the
     autocompletion AJAX endpoint. That function, when called with a search
     term, returns a list of matching file paths.
     """
-    def _query(term):
-        url = '/dajaxice/tncapp.files_autocomplete/'
+    def _query(term, url, key):
         payload = {'search_term': term}
         data = {'argv': json.dumps(payload)}
         response = client.post(url, data=urllib.urlencode(data),
@@ -49,7 +50,7 @@ def get_completions(client, files_test_data):
                                HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         response_data = json.loads(response.content)
         results = response_data['results']
-        return [r['text'] for r in results]
+        return [r[key] for r in results]
     return _query
 
 
@@ -78,5 +79,28 @@ def get_completions(client, files_test_data):
     ('/saberthoothtigerdinozord', [])
 ])
 def test_files_autocomplete(get_completions, search_term, expected):
-    results = get_completions(search_term)
+    results = get_completions(search_term, '/dajaxice/tncapp.files_autocomplete/', 'file')
+    assert sorted(results) == sorted(expected)
+
+
+@pytest.mark.parametrize('search_term, expected', [
+    # 01
+    ('/bin', ['/bin', '/usr/bin']),
+    # 02
+    ('usr', ['/usr', '/usr/bin', '/usr/lib']),
+    # 03
+    ('etc', ['/etc', '/system/lib/etc']),
+    # 04
+    ('tem', ['/system/lib/etc']),
+    ('lib', ['/system/lib/etc', '/usr/lib']),
+    # 05
+    ('/etc', ['/etc', '/system/lib/etc']),
+    # 06
+    ('/', ['/', '/bin', '/usr', '/usr/bin',
+           '/etc', '/system/lib/etc', '/usr/lib']),
+    # No results
+    ('/saberthoothtigerdinozord', [])
+])
+def test_directory_autocomplete(get_completions, search_term, expected):
+    results = get_completions(search_term, '/dajaxice/tncapp.directories_autocomplete/', 'directory')
     assert sorted(results) == sorted(expected)
