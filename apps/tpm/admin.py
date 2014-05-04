7e2792d937120cac4d233b7c749d5edc9ba4b119
Danilo Bargen <mail@dbrgn.ch>
2014-05-04 16:16:55 +0200

    Separated tpm app

---
+++ apps/tpm/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+from django.contrib import admin
+
+from . import models
+
+
+admin.site.register(models.Component)
+admin.site.register(models.ComponentHash)
---
+++ apps/tpm/models.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+from django.db import models
+
+from tncapp.models import HashField
+
+
+class Component(models.Model):
+    vendor_id = models.IntegerField()
+    name = models.IntegerField()
+    qualifier = models.IntegerField(default=0)
+    label = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'components'
+
+
+class ComponentHash(models.Model):
+    # TODO missing "id" primary key in database
+    component = models.ForeignKey(Component)
+    device = models.ForeignKey('tncapp.Device', db_column='key')
+    seq_no = models.IntegerField()
+    pcr = models.IntegerField()
+    algorithm = models.ForeignKey('tncapp.Algorithm', db_column='algo')
+    hash = HashField()
+
+    class Meta:
+        db_table = 'component_hashes'
+        verbose_name_plural = 'component hashes'
