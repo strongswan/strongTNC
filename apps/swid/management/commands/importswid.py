48131b35bed7c45fc9baddbd9c37899c28cbf9c4
Danilo Bargen <mail@dbrgn.ch>
2014-05-04 14:47:39 +0200

    Status output for importswid command

--- apps/swid/management/commands/importswid.py
+++ apps/swid/management/commands/importswid.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function, division, absolute_import, unicode_literals
-
 """
 Custom manage.py command to import swid tags from a file.
 
@@ -8,7 +6,9 @@ Usage: ./manage.py importswid [filename]
 
 The file must contain valid swid tags, one swid tag per line, separated by a **single** newline.
 """
+from __future__ import print_function, division, absolute_import, unicode_literals
 
+import sys
 import os.path
 
 from django.core.management.base import BaseCommand, CommandError
@@ -25,13 +25,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         if len(args) != 1:
-            raise CommandError('Wrong argument count, command needs exactly one argument.')
+            raise CommandError('Usage: ./manage.py importswid <filename>')
 
         filename = args[0]
 
         if not os.path.isfile(filename):
             raise CommandError('No such file: ' + filename)
 
+        encoding = self.stdout.encoding or 'ascii'
         with open(filename, 'r') as f:
             for line in f:
-                utils.process_swid_tag(line.strip())
+                tag_xml = line.strip().decode('utf8')
+                tag = utils.process_swid_tag(tag_xml)
+                self.stdout.write('Processed {0}'.format(tag).encode(encoding, 'replace'))
