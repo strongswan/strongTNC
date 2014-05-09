4ce624c31a89ff8edb3f4ba4dbe7fae9e193dc2e
cfaessler <cfaessle@hsr.ch>
2014-05-09 16:04:47 +0200

    Added swid measurement controller

--- apps/core/viewsets.py
+++ apps/core/viewsets.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 
 from . import models
+from apps.swid import models as swid_models
+from apps.core import models as core_models
 from . import serializers
 
 
@@ -15,3 +20,37 @@ class IdentityViewSet(viewsets.ReadOnlyModelViewSet):
 class SessionViewSet(viewsets.ReadOnlyModelViewSet):
     model = models.Session
     serializer_class = serializers.SessionSerializer
+
+    @action()
+    def swid_measurement(self, request, pk=None):
+        """"
+        Link the given software-ids with the current session.
+        If no corresponding tag is available for one or more software-ids, return these software-ids
+        with HTTP status code 412 Precondition failed.
+
+        """
+        software_ids = request.DATA
+        found_tags = {}
+        missing_tags = []
+
+        # look for matching tags
+        for tag in swid_models.Tag.objects.all():
+            ids = tag.get_software_ids()
+            for software_id in software_ids:
+                if software_id in ids:
+                    found_tags[software_id] = tag
+
+        for software_id in software_ids:
+            if software_id not in found_tags:
+                missing_tags.append(software_id)
+
+        # not all tags for the given software-ids are available, client should create them first
+        if missing_tags:
+            return Response(data=missing_tags, status=status.HTTP_412_PRECONDITION_FAILED)
+
+        # all tags are available; link them with a session
+        else:
+            session = core_models.Session.objects.get(pk=pk)
+            session.tag_set.add(*found_tags.values())
+            session.save()
+            return Response()
---
+++ tests/test_api.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+import pytest
+
+from django.contrib.auth.models import User
+
+from rest_framework.test import APIClient
+
+from .test_swid import swidtag
+
+
+@pytest.fixture
+def api_client(transactional_db):
+    user = User.objects.create_superuser(username='api-test', password='api-test',
+                                         email="api-test@example.com")
+    user.is_staff = True
+    user.save()
+    client = APIClient()
+    client.force_authenticate(user=user)
+    return client
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('filename', [
+    'strongswan.short.swidtag'])
+def test_swid_measurement_diff(api_client, swidtag, filename):
+    software_ids = [
+        'regid.2004-03.org.strongswan_cowsay',
+        'regid.2004-03.org.strongswan_debian_7.4-x86_64-strongswan-4.5.2-1.5+deb7u3'
+    ]
+
+    response = api_client.post('/api/sessions/2/swid_measurement/', software_ids, format='json')
+    assert len(response.data) == 1
+    assert software_ids[1] not in response
+    assert software_ids[0] in response
