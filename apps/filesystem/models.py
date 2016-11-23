881c59303c8935cfa7dd4e858e934f17d8583d2f
Tobias Brunner <tobias@strongswan.org>
2016-11-23 12:03:29 +0100

    File hashes currently don't have a device assigned

--- apps/filesystem/models.py
+++ apps/filesystem/models.py
@@ -111,7 +111,7 @@ class FileHash(models.Model):
     """
     file = models.ForeignKey(File, on_delete=models.CASCADE, db_column='file')
     product = models.ForeignKey('devices.Product', db_column='product')
-    device = models.IntegerField(null=False, default=0)  # TODO convert to nullable(?) FK
+    device = models.ForeignKey('devices.Device', null=True, blank=True)
     algorithm = models.ForeignKey(Algorithm, db_column='algo', on_delete=models.PROTECT)
     hash = HashField(db_column='hash')
 
