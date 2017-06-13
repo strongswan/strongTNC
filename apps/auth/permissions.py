strongswan/strongTNC

commit 9fcdf556cc96e254058a495693bdc02e0775fc46

Update to Django 1.8 LTS

apps/auth/permissions.py
@@ -41,11 +41,12 @@ class GlobalPermission(Permission):
 
     class Meta:
         proxy = True
+        verbose_name = 'global_permission'
 
     def save(self, *args, **kwargs):
         # Ensure that a content type called 'global_permission' exists
         ct, created = ContentType.objects.get_or_create(
-            name='global_permission', app_label='auth'
+            model=self._meta.verbose_name, app_label='auth'
         )
         # Assign the 'global_permission' content type to this permission
         self.content_type = ct

config/settings.py
@@ -223,6 +223,12 @@ INSTALLED_APPS = (
 if DEBUG_TOOLBAR:
     INSTALLED_APPS += ('debug_toolbar',)
 
+# Ignore migrations in the auth module as our virtual permission model can't
+# have any
+MIGRATION_MODULES = {
+    'auth': "contrib.auth.migrations_not_used_in_tests",
+}
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.

tests/test_legacy.py
@@ -14,7 +14,7 @@ from apps.filesystem.models import File, Directory
 
 class TncappTest(TestCase):
     def setUp(self):
-        user = Identity.objects.create(data='Test User', type=1)
+        user = Identity.objects.create(pk=1, data='Test User', type=1)
         user.save()
 
         p = Product.objects.create(name='Fancy OS 3.14')

tests/test_packages.py
@@ -19,7 +19,7 @@ from .fixtures import *  # NOQA: Star import is OK here because it's just a test
 @pytest.fixture
 def package_testdata(transactional_db):
     package = mommy.make(Package, pk=1)
-    product = mommy.make(Product)
+    product = mommy.make(Product, pk=1)
     time = timezone.now()
     mommy.make(Version, pk=1, product=product, package=package, release='1.0', security=True, blacklist=True,
                time=time)

commit e313f6a36552af5dcd8b4a0fd666a63f56fe7b52

Fix filter in custom permission manager
Fixes: 9fcdf556cc96 ("Update to Django 1.8 LTS")

apps/auth/permissions.py
@@ -29,7 +29,7 @@ class GlobalPermissionManager(models.Manager):
     """
     def get_query_set(self):
         return super(GlobalPermissionManager, self).\
-            get_query_set().filter(content_type__name='global_permission')
+            get_query_set().filter(content_type__model='global_permission')
 
 
 class GlobalPermission(Permission):