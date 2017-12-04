53aee20b9d3ac9fa24f66ac7d63111f4409b6b90
Tobias Brunner <tobias@strongswan.org>
2017-12-04 16:13:11 +0100

    Add CSRF token to all jQuery AJAX requests that require it

--- apps/front/static/js/common.js
+++ apps/front/static/js/common.js
@@ -112,3 +112,30 @@ var GlobalAjaxIndicator = {
     }
 };
 
+function getCookie(name) {
+    var value = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                value = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return value;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
+        }
+    }
+});
