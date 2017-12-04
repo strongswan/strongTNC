43f02a76b72f0ada55c39d1b5f131a5ec72d29e6
Tobias Brunner <tobias@strongswan.org>
2017-12-04 19:02:40 +0100

    Return 403 Permission Denied error for unauthenticated AJAX requests

--- apps/core/decorators.py
+++ apps/core/decorators.py
@@ -2,10 +2,7 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
 
 from functools import wraps
-
-
-class AuthenticationRequiredError(RuntimeError):
-    pass
+from django.core.exceptions import PermissionDenied
 
 
 def ajax_login_required(func):
@@ -13,6 +10,6 @@ def ajax_login_required(func):
     def __wrapper(request, *args, **kwargs):
         # Check authentication
         if not request.user.is_authenticated():
-            raise AuthenticationRequiredError()
+            raise PermissionDenied()
         return func(request, *args, **kwargs)
     return __wrapper
