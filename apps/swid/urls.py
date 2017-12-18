d18595f73d4416e370a0b95df645fa5e3ba9edc2
Andreas Steffen <andreas.steffen@strongswan.org>
2017-12-18 11:55:55 +0100

    swid: Removed appended slash

--- apps/swid/urls.py
+++ apps/swid/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     url(r'^swid-tags/$', views.SwidTagListView.as_view(), name='tag_list'),
     url(r'^swid-tags/(?P<pk>\d+)/$', views.SwidTagDetailView.as_view(), name='tag_detail'),
     url(r'^swid-inventory/(?P<pk>\d+)/$', views.SwidInventoryView.as_view(), name='inventory'),
-    url(r'^swid-inventory/stats/$', ajax.get_tag_inventory_stats, name='tag_inventory_stats'),
+    url(r'^swid-inventory/stats$', ajax.get_tag_inventory_stats, name='tag_inventory_stats'),
     url(r'^swid-log/(?P<pk>\d+)/$', views.SwidLogView.as_view(), name='log'),
-    url(r'^swid-log/stats/$', ajax.get_tag_log_stats, name='tag_log_stats'),
-    url(r'^session-info/$', ajax.session_info, name='session_info'),
+    url(r'^swid-log/stats$', ajax.get_tag_log_stats, name='tag_log_stats'),
+    url(r'^session-info$', ajax.session_info, name='session_info'),
 ]
