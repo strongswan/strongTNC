strongswan/strongTNC

commit f6e3b39457b5e76e20da3124ecf85b255c02847e

Updated MIGRATION_MODULES for auth

config/settings.py
@@ -247,7 +247,7 @@ if DEBUG_TOOLBAR:
 # Ignore migrations in the auth module as our virtual permission model can't
 # have any
 MIGRATION_MODULES = {
-    'auth': "contrib.auth.migrations_not_used_in_tests",
+    'auth': None,
 }
 
 # A sample logging configuration. The only tangible logging

commit 7cb55a374755e1c26a3418b69d07892c8f1d4cd5

Only exclude migrations for auth app in tests
The previous fix disabled applying migrations for this app altogether,
not only for tests.

Fixes: f6e3b39457b5 ("Updated MIGRATION_MODULES for auth")
Closes #19.

config/settings.py
@@ -244,12 +244,6 @@ INSTALLED_APPS = (
 if DEBUG_TOOLBAR:
     INSTALLED_APPS += ('debug_toolbar',)
 
-# Ignore migrations in the auth module as our virtual permission model can't
-# have any
-MIGRATION_MODULES = {
-    'auth': None,
-}
-
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.

runtests.py
@@ -6,7 +6,7 @@ import coverage
 
 if __name__ == '__main__':
     # Environment variables
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings_tests')
 
     measure_coverage = True
     if '--no-cov' in sys.argv:

config/settings_tests.py
@@ -1,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Django settings for strongTNC during tests
+
+# Import base settings
+from settings import *
+
+# Ignore migrations in the auth module as our virtual permission model can't
+# have any
+MIGRATION_MODULES = {
+    'auth': None,
+}